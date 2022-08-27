0110357e530dac3522d3855e9e4ce3130183a87b
Danielgamer9799 <110137940+danielgamer9799@users.noreply.github.com>
2022-08-27 14:57:21 -0400

    Update loader.lua

--- Dans/loader.lua
+++ Dans/loader.lua
@@ -1,1 +1,11 @@
 --loader for the script
+local games = {
+    ["4924922222"] = "",
+    ["6839171747"] = ""
+}
+
+for id, url in next, games do
+    if string.find(id, game.PlaceId) then
+        loadstring(game:HttpGet(url))(); break
+    end
+end
