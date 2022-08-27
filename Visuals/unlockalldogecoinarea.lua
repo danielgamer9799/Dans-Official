e57c7f4e1f3e514cfc44838ad1ce379692c73ff3
Danielgamer9799 <110137940+danielgamer9799@users.noreply.github.com>
2022-08-27 13:30:47 -0400

    Update unlockalldogecoinarea.lua

--- Visuals/unlockalldogecoinarea.lua
+++ Visuals/unlockalldogecoinarea.lua
@@ -1,1 +1,8 @@
 game.Workspace.LevelDoors:Destroy()
+
+game.StarterGui:SetCore("SendNotification", {
+	Title = "loaded unlock all level doors"; 
+	Text = "@danielgamer9799 in scriptblox.com"; 
+	Icon = "http://www.roblox.com/asset/?id=54952350";
+	Duration = "5";
+})
