abd2c7417808eccc491276a7fb316e805d9e3904
Danielgamer9799 <110137940+danielgamer9799@users.noreply.github.com>
2022-09-27 17:58:02 -0400

    Update NINJALEGENDSUNLOCKALL.lua

--- Visuals/ClonedStuff/NINJALEGENDSUNLOCKALL.lua
+++ Visuals/ClonedStuff/NINJALEGENDSUNLOCKALL.lua
@@ -1,1 +1,58 @@
+local Player = game.Players.LocalPlayer
 
+game.StarterGui:SetCore("SendNotification", {
+	Title = "Unlocking All Islands";
+	Text = "see the magic happen :)";
+	Icon = "http://www.roblox.com/asset/?id=2459243309";
+	Button1 = "ok"; 
+	Duration = "7";
+})
+
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Enchanted Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Astral Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Mystical Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Space Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Tundra Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Eternal Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Sandstorm"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Thunderstorm"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Ancient Inferno Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Midnight Shadow Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Mythical Souls Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Winter Wonder Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Golden Master Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Dragon Legend Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Cybernetic Legends Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Skystorm Ultraus Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Chaos Legends Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Soul Fusion Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Dark Elements Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Inner Peace Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Blazing Vortex Island"].islandSignPart.CFrame
+game.StarterGui:SetCore("SendNotification", {
+	Title = "Unlocked All Islands";
+	Text = "you can do this multiple times";
+	Icon = "http://www.roblox.com/asset/?id=419589574";
+	Button1 = "ok"; 
+	Duration = "7";
+})
