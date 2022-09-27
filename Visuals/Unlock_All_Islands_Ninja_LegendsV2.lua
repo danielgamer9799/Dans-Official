7a98b65ef7dbfc502b6ce80967e65031b4764d49
Danielgamer9799 <110137940+danielgamer9799@users.noreply.github.com>
2022-09-27 17:46:15 -0400

    Create Unlock_All_Islands_Ninja_LegendsV2.lua

---
+++ Visuals/Unlock_All_Islands_Ninja_LegendsV2.lua
@@ -0,0 +1,42 @@
+local Player = game.Players.LocalPlayer
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Enchanted Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Astral Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Mystical Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Space Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Tundra Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Eternal Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Sandstorm"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Thunderstorm"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Ancient Inferno Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Midnight Shadow Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Mythical Souls Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Winter Wonder Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Golden Master Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Dragon Legend Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Cybernetic Legends Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Skystorm Ultraus Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Chaos Legends Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Soul Fusion Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Dark Elements Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Inner Peace Island"].islandSignPart.CFrame
+wait(1.5)
+Player.Character.HumanoidRootPart.CFrame = game.Workspace.islandUnlockParts["Blazing Vortex Island"].islandSignPart.CFrame
