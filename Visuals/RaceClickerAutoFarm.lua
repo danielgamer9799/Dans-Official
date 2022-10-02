c9e7990fbd3318fa5ebb68c78eff7a611eb093cf
Danielgamer9799 <110137940+danielgamer9799@users.noreply.github.com>
2022-10-02 17:49:38 -0400

    Create RaceClickerAutoFarm.lua

---
+++ Visuals/RaceClickerAutoFarm.lua
@@ -0,0 +1,32 @@
+local Player = game.Players.LocalPlayer
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage01.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage02.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage03.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage04.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage05.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage06.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage07.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage08.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage09.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage10.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage25.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage50.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage250.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage100.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage500.Sign.CFrame
+wait(0.4)
+Player.Character.HumanoidRootPart.CFrame = workspace.LoadedWorld.Track.Stage1K.Sign.CFrame
